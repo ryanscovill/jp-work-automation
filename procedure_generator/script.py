2b2a8925ed30b2c7ce1da03ca23460238ef22477
ryanscovill <rscovill@sfu.ca>
2023-10-03 15:55:53 -0700

    fix typo

--- procedure_generator/script.py
+++ procedure_generator/script.py
@@ -31,7 +31,7 @@ num_work_procedure_pages = 3
 # Extracts the data from a pdf into a dictionary
 def extract_fillable_data(pdf_path) -> dict:
     fields = fillpdfs.get_form_fields(pdf_path)
-    fields = {k: v for k, v in fields.items() if "Checkbox" not in k.lower()}
+    fields = {k: v for k, v in fields.items() if "checkbox" not in k.lower()}
 
     return fields
 
@@ -160,7 +160,7 @@ def setDebug(args):
     'menuTitle': 'About',
     'name': 'Work Procedure PDF Generator',
     'description': 'Automate creating a work procedure PDF',
-    'version': '1.4.0',
+    'version': '1.4.1',
     'copyright': '2023',
     'website': 'https://github.com/ryanscovill',
     'license': 'MIT'
